b5c4a462c5cabd0c68059603e6b8100afe9e22bc
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-06-21 22:28:38 -0300

    Realizada correções

--- src/DiscoRigido.java
+++ src/DiscoRigido.java
@@ -4,30 +4,32 @@ public class DiscoRigido {
 
 	static int tamanhoDoDiscoDisponivel = 40; // TAMANHO TOTAL 40MB.
 	static Bloco[] blocos = new Bloco[4];// DIVIDIDOS EM 4 BLOCOS DE 10MB.
-	
-	//CONTRUTOR.
+
+	// CONTRUTOR.
 	public DiscoRigido() {
 		criarBlocos();
 	}
-	
-	//INSTANCIA BLOCOS.
-	public void criarBlocos() {
+
+	// INSTANCIA BLOCOS.
+	private void criarBlocos() {
 		for (int i = 0; i < 4; i++) {
 			Bloco b = new Bloco();
 			b.setNome("BLOCO 0" + (i + 1));
 			blocos[i] = b;
 		}
 	}
-	//CRIA UM DIRETÓRIO E ADICIONA A UM BLOCO VAZIO.
+
+	// CRIA UM DIRETÓRIO E ADICIONA A UM BLOCO VAZIO.
 	public static void adicionarDiretorio(String nome) {
 		Diretorio d = new Diretorio(nome);
 		adicionarDiretorioAoBloco(d);
 
 	}
-	//REMOVE O DIRETÓRIO RECEBENDO O NOME.
+
+	// REMOVE O DIRETÓRIO RECEBENDO O NOME.
 	public void removerDiretorio(String nome) {
 		if (removerDiretorioNoBloco(nome)) {
-			System.out.println("DIRETORIO REMOVIDO COM SUCESSO!");
+			System.out.println("DIRETÓRIO " + nome + " REMOVIDO COM SUCESSO!");
 			imprimeEspacoDisponivelNoDisco();
 		} else {
 			System.out
@@ -35,22 +37,28 @@ public class DiscoRigido {
 			listarDiretorios();
 		}
 	}
-	//ADICIONAR DIRETÓRIO DENTRO DE UM BLOCO VAZIO.
+
+	// ADICIONAR DIRETÓRIO DENTRO DE UM BLOCO VAZIO.
 	private static void adicionarDiretorioAoBloco(Diretorio diretorio) {
-		for (int i = 0; i < 4; i++) {
-			if (blocos[i].espacoRestante >= 10) {
-				diretorio.setNomeDoBloco(blocos[i].getNome());
-				blocos[i].setDiretorio(diretorio);
-				blocos[i].setEspacoRestante(blocos[i].getEspacoRestante() - 1);
-				tamanhoDoDiscoDisponivel -= 1;
-				System.out.println(
-						"DIRETÓRIO CRIADO NOME: " + diretorio.getNome() + " INCLUIDO NA RAIZ, " + blocos[i].getNome());
-				imprimeEspacoDisponivelNoDisco();
-				break;
+		if (verificarNoDiretorio(diretorio)) {
+			for (int i = 0; i < 4; i++) {
+				if (blocos[i].espacoRestante >= 10) {
+					diretorio.setNomeDoBloco(blocos[i].getNome());
+					blocos[i].setDiretorio(diretorio);
+					blocos[i].setEspacoRestante(blocos[i].getEspacoRestante() - diretorio.getTamanho());
+					tamanhoDoDiscoDisponivel -= diretorio.getTamanho();
+					System.out.println("DIRETÓRIO CRIADO NOME: " + diretorio.getNome() + " INCLUIDO NA RAIZ, "
+							+ blocos[i].getNome());
+					imprimeEspacoDisponivelNoDisco();
+					break;
+				}
 			}
-		}
+		} else
+			System.out.println("FALHA AO TENTAR CRIAR O DIRETÓRIO: " + diretorio.getNome() + ", DIRETÓRIO JÁ EXISTE.");
+		listarDiretoriosEArquivos();
 	}
-	//REMOVE O DIRETÓRIO NO BLOCO ONDE FOR ENCONTRADO.
+
+	// REMOVE O DIRETÓRIO NO BLOCO ONDE FOR ENCONTRADO.
 	private boolean removerDiretorioNoBloco(String nome) {
 		for (int i = 0; i < 4; i++) {
 			if (blocos[i].getEspacoRestante() != 10) {
@@ -59,7 +67,8 @@ public class DiscoRigido {
 						if (d.getNome().equals(nome)) {
 							blocos[i].getListaDeDiretorios().remove(d);
 
-							tamanhoDoDiscoDisponivel += 1;
+							tamanhoDoDiscoDisponivel += d.getTamanho();
+							blocos[i].setEspacoRestante(blocos[i].getEspacoTotal());
 							return true;
 						}
 					}
@@ -82,14 +91,14 @@ public class DiscoRigido {
 		}
 		imprimeEspacoDisponivelNoDisco();
 	}
-	
-	//IMPRIME O ESPAÇO DISPONÍVEL EM DISCO.
+
+	// IMPRIME O ESPAÇO DISPONÍVEL EM DISCO.
 	public static void imprimeEspacoDisponivelNoDisco() {
 		fragmentacao();
 		System.out.println("ESPAÇO DISPONÍVEL NO DISCO: " + tamanhoDoDiscoDisponivel + "MB.\n");
 	}
-	
-	//CRIAR ARQUIVO E ALOCAR EM UM DIRETÓRIO.
+
+	// CRIAR ARQUIVO E ALOCAR EM UM DIRETÓRIO.
 	public static void criarArquivo(String nome, int tamanho, String nomeDiretorio) {
 		Arquivo arquivo = new Arquivo(nome, tamanho, nomeDiretorio);
 		if (arquivo.getTamanho() > 9 || arquivo.getTamanho() <= 0) {
@@ -97,25 +106,36 @@ public class DiscoRigido {
 		} else
 			armazenarArquivo(arquivo);
 	}
-	//ARMAZENAR UM ARQUIVO NO DIRETÓRIO.
+
+	// ARMAZENAR UM ARQUIVO NO DIRETÓRIO.
 	private static boolean armazenarArquivo(Arquivo arquivo) {
+
 		for (int i = 0; i < 4; i++) {
 			if (blocos[1].getListaDeDiretorios() != null) {
 				for (Diretorio d : blocos[i].getListaDeDiretorios()) {
 					if (d.getNome().equals(arquivo.getNomeDiretorio())) {
-						d.setArquivo(arquivo);
-						d.setTamanho(d.getTamanho() + arquivo.getTamanho());
-						blocos[i].setArquivos(arquivo);
-						blocos[i].setEspacoRestante(blocos[i].getEspacoRestante() - arquivo.getTamanho());
-						tamanhoDoDiscoDisponivel -= arquivo.getTamanho();
-						System.out.println("ARQUIVO: " + arquivo.getNome() + ", ADICIONADO COM SUCESSO NO DIRETÓRIO: "
-								+ d.getNome() + ", LOCALIZADO NA RAIZ, " + blocos[i].getNome());
-						imprimeEspacoDisponivelNoDisco();
-						return true;
+						if (verificarNomeArquivo(arquivo)) {
+							d.setArquivo(arquivo);
+							d.setTamanho(d.getTamanho() + arquivo.getTamanho());
+							blocos[i].setArquivos(arquivo);
+							blocos[i].setEspacoRestante(blocos[i].getEspacoRestante() - arquivo.getTamanho());
+							tamanhoDoDiscoDisponivel -= arquivo.getTamanho();
+							System.out.println("ADICIONADO O ARQUIVO: " + arquivo.getNome() + ", DE TAMANHO: "
+									+ arquivo.getTamanho() + "MB, ADICIONADO COM SUCESSO NO DIRETÓRIO: " + d.getNome()
+									+ ", LOCALIZADO NA RAIZ, " + blocos[i].getNome());
+							imprimeEspacoDisponivelNoDisco();
+							return true;
+						}
+						System.out.println(
+								"ARQUIVO NÃO FOI CRIADO/COPIADO, NOME INVÁLIDO, JÁ EXISTE UM ARQUIVO COM MESMO NOME.");
+						return false;
 					}
+
 				}
 			}
+
 		}
+
 		System.out.println("ARQUIVO NÃO FOI CRIADO/COPIADO, DIRETÓRIO INVALIDO,"
 				+ " SE NÃO SOUBER O DIRETÓRIO DIGITE CONFIRA A LISTA DE DIRETÓRIOS DISPONÍVEIS.");
 		listarDiretorios();
@@ -143,11 +163,12 @@ public class DiscoRigido {
 			}
 		}
 		System.out.println("ARQUIVO NÂO ENCONTRADO, VERIFIQUE SE O NOME DO ARQUIVO ESTA CORRETO.");
+
 		return false;
 	}
-	
-	//LISTAR TODOS OS DIRETORIOS E ARQUIVOS.
-	public void listarDiretoriosEArquivos() {
+
+	// LISTAR TODOS OS DIRETORIOS E ARQUIVOS.
+	public static void listarDiretoriosEArquivos() {
 		System.out.println("LISTA DE DIRETÓRIOS E ARQUIVOS:");
 		for (int i = 0; i < 4; i++) {
 			if (blocos[i].getListaDeDiretorios() != null) {
@@ -168,8 +189,8 @@ public class DiscoRigido {
 		}
 		imprimeEspacoDisponivelNoDisco();
 	}
-	
-	//VERIFICAR FRAGMENTAÇÃO NOS BLOCOS.
+
+	// VERIFICAR FRAGMENTAÇÃO NOS BLOCOS.
 	public static void fragmentacao() {
 		int aux = 0;
 		for (int i = 0; i < 4; i++) {
@@ -179,4 +200,31 @@ public class DiscoRigido {
 		}
 		System.out.println("FRAGMENTAÇÃO INTERNA: " + aux);
 	}
+
+	// VERIFICAR SE O DIRETÓRIO TEM O NOME IGUAL.
+	private static boolean verificarNoDiretorio(Diretorio diretorio) {
+		for (int i = 0; i < 4; i++) {
+			for (Diretorio d : blocos[i].getListaDeDiretorios()) {
+				if (d.getNome().equals(diretorio.getNome())) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// VERIFICAR SE EXISTE ARQUIVO COM NOME IGUAL.
+	private static boolean verificarNomeArquivo(Arquivo arquivo) {
+		for (int i = 0; i < 4; i++) {
+			for (Diretorio d : blocos[i].getListaDeDiretorios()) {
+				for (Arquivo a : blocos[i].listaDeArquivos) {
+					if (a.getNome().equals(arquivo.getNome())) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
 }
--- src/Programa.java
+++ src/Programa.java
@@ -8,21 +8,33 @@ public class Programa {
 		//ADICIONAR/CRIAR DIRETÓRIOS
 		hd.adicionarDiretorio("PASTA 1");
 		hd.adicionarDiretorio("PASTA 2");
+		hd.adicionarDiretorio("PASTA 3");
 		
 		//REMOVER DIRETORIO NÃO EXISTENTE
 		hd.removerDiretorio("testes");
+		hd.removerDiretorio("PASTA 1");
 		
 		
 		//CRIAR ARQUIVOS EM DIRETÓRIOS
 		hd.criarArquivo("FOTOS JOAO", 2, "PASTA 1");
+		hd.removerDiretorio("PASTA 1");
 		hd.criarArquivo("FOTOS MARIA", 5, "PASTA 2");
+		hd.criarArquivo("FOTOS MARIA", 5, "PASTA 2");
+		
+
 		
 		//LISTAR DIRETÓRIOS E ARQUIVOS
 		hd.listarDiretoriosEArquivos();
 		
 		//REMOVER ARQUIVO
 		hd.removerArquivo("FOTOS JOAO");
+		hd.removerArquivo("FOTOS JOAO");
+		
+		
+		
 		hd.listarDiretoriosEArquivos();
+		
+		hd.adicionarDiretorio("PASTA 1");
 
 	}
 
